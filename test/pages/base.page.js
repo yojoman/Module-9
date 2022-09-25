a209f0196eaed172d255fa1be5876588ae427327
Pavel Abramuk <pavel.abramuk5@gmail.com>
2022-09-25 08:57:18 +0300

    versatile page title selector added

--- test/pages/base.page.js
+++ test/pages/base.page.js
@@ -2,8 +2,12 @@ module.exports = class BasePage {
   async openPage() {
     return await browser.url("/");
   }
-  
+
   getButton(buttonName) {
     return $(`.//a[contains(text(),'${buttonName}')]`);
+  }
+
+  getCommonPageTitle(title) {
+    return $(`.//h1[contains(text(),"${title}")]`);
   }
 };
