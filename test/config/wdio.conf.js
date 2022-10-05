50e816e97653a3e5916a63db75aca168de88cde2
Pavel Abramuk <pavel.abramuk5@gmail.com>
2022-10-05 19:03:36 +0300

    adjusted waiting timeout

--- test/config/wdio.conf.js
+++ test/config/wdio.conf.js
@@ -96,7 +96,7 @@ exports.config = {
   baseUrl: "https://www.onliner.by/",
   //
   // Default timeout for all waitFor* commands.
-  waitforTimeout: 10000,
+  waitforTimeout: 15000,
   //
   // Default timeout in milliseconds for request
   // if browser driver or grid doesn't send response
