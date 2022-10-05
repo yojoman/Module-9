3ef755b35b60c9110fd0a17da6f21617d3d31f00
Pavel Abramuk <pavel.abramuk5@gmail.com>
2022-10-05 19:04:33 +0300

    small adjustments

--- test/pages/main.page.js
+++ test/pages/main.page.js
@@ -1,16 +1,17 @@
 const BasePage = require("./base.page.js");
+const commonPage = require("./common.page.js");
 
 class MainPage extends BasePage {
+  get firstSearchField() {
+    return $(".//ul/li[@class='search__result'][1]");
+  }
+
   get headerMenu() {
     return $$('.//span[contains(@class,"b-main-navigation__text")]');
   }
 
   get footerMenu() {
     return $$('.//a[contains(@class,"footer-style__link footer-style__link")]');
-  }
-
-  get catalogMenuButton() {
-    return $('.//span[contains(text(),"Каталог")] [@class="b-main-navigation__text"]')
   }
 
   getHeaderMenuText() {
@@ -21,6 +22,18 @@ class MainPage extends BasePage {
   getFooterMenuText() {
     const elements = this.footerMenu.map((element) => element.getText());
     return elements;
+  }
+
+  async typeInSearhField(searchText) {
+    await commonPage.searchInputField.waitForDisplayed();
+    await commonPage.searchInputField.setValue(searchText);
+  }
+  async openFirstItemFromSearchField() {
+    const searchIframe = await browser.$(".//iframe[@class='modal-iframe']");
+    browser.switchToFrame(searchIframe);
+    await this.firstSearchField.waitForDisplayed();
+    await this.firstSearchField.click();
+    browser.switchToParentFrame();
   }
 }
 
